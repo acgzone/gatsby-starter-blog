e826b6a726f585aff62e867aa769d5a5e177ee01
znq <znq@moondeMacBook-Pro.local>
2017-07-09 12:07:24 +0800

    调整底部样式

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,58 +10,35 @@ import './footer.css'
 
 const Footer = () => {
   return <div>
-        <img
-          src={require('./footer.png')}
-          alt={`大成日语`}
-          style={{
-            marginRight: rhythm(3 / 4),
-            marginBottom: 0,
-            width: '100%'
-          }}
-        />
-        <div className="nav">
-          <Link
-            to="/"
-            css={{
-              display: `inline-block`,
-              textDecoration: `none`,
-              marginRight: rhythm(2/4)
-            }}
-          >
-          关于我们
-        </Link>
-          <Link
-            to="/"
-            css={{
-              display: `inline-block`,
-              textDecoration: `none`,
-              marginRight: rhythm(2/4)
-            }}
-          >
-          人才招聘
-        </Link>
-          <Link
-            to="/"
-            css={{
-              display: `inline-block`,
-              textDecoration: `none`,
-              marginRight: rhythm(2/4)
-            }}
-          >
-          联系我们
-        </Link>
+      <div className="bg">
+        <div className="content">
+          <img src={require('./bg_text.png')}/>
         </div>
-        <div className="contact">
-          <p><span>地址</span>:<span>成都市高新区天府二街复城国际</span> </p>
-          <p><span>电话</span>:<span>i028-66666666</span> </p>
-          <p><span>邮箱</span>:<span>superman@dacheng.com</span> </p>
-          <p><span>法律顾问</span>:<span>北京xxxx法律事务所</span> </p>
+      </div>
+      <div>
+        <div className="footer"></div>
+        <div className="footer_inner">
+          <div className="contact">
+            <div className="text">
+              <text>电话：</text>
+              <text>028 – 84278169</text>
+            </div>
+            <div className="text">
+              <text>邮箱：</text>
+              <text>396703883@qq.com</text>
+            </div>
+            <div className="text">
+              <text>地址：</text>
+              <text>成都市锦江区宏济新路5号  SOHO商务港309</text>
+            </div>
+          </div>
+          <div className="copyright">
+            <div>Copyright@2006-2017</div>
+            <div>版权所属成都大成教育公司 未经许可 严禁复制</div>
+            <div>蜀ICP备13423432423号</div>
+          </div>
         </div>
-        <div className="copyright">
-          <p>Copyright@2006-2017</p>
-          <p>版权所属成都大成教育公司 未经许可 严禁复制</p>
-          <p>蜀ICP备13423432423号</p>
-        </div>
+      </div>
     </div>
 }
 
