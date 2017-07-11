2b550c91239d88152244c041c68d064e649c876d
znq <znq@moondeMacBook-Pro.local>
2017-07-11 21:19:14 +0800

    关于大成

--- src/pages/aboutDc/index.jsx
+++ src/pages/aboutDc/index.jsx
@@ -9,27 +9,34 @@ class Index extends React.Component {
   render() {
 
     return (
-      <div className="abouts">
-        <div className="about">
-            <img src={require('./27profile_01.png')} className="about-img"></img>
-            <h2 className="about-title">关于大成</h2>
-            <div className="about-neirong">
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;大成日语专注于日语教育培训、日本留学咨询，是专业的日语学习机构。始终致力于为学员提供更优质、更高效的一站式日语学习及日本留学服务。目前，大成日语旗下开设有入门日语、等级日语、旅游日语、少儿日语、留学日语、商务日语等各种课程，能够满足有不同日语学习需求的各类人群。
-            </div>
-            <img src={require('./28profile_02.png')} className="about-img"></img>
-            <h2 className="about-title">日本留学专家</h2>
-            <div className="about-neirong">
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;大成日语与日本东京、京都、大阪等地的多所优良语言学校有长期良好的合作，输送了许多优秀的中国学生赴日留学。针对于中学生、大学生、研究生等不同阶段的同学，制定有多种个性化的专业的留学申请方案。
+      <div>
+        <div className="about-daohang">
+            <div className="about-daohang1">
+                <img src={require('./about us.png')} className="about-daohang2"></img><span>首页</span> &gt;<span>日语课程</span>
             </div>
         </div>
-        <div className="about-u">
-            <ul className="about-ul">
-              <li className="about-li">关于大成</li>
-              <Link to="/aboutDc"><li className="about-lis">大成介绍</li></Link>
-              <Link to="/aboutDc/about1"><li className="about-lis">团队介绍</li></Link>
-              <Link to="/aboutDc/about2"><li className="about-lis">教学环境</li></Link>
-            </ul>
+        <div className="abouts">
+          <div className="about">
+              <img src={require('./27profile_01.png')} className="about-img"></img>
+              <h2 className="about-title">关于大成</h2>
+              <div className="about-neirong">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;大成日语专注于日语教育培训、日本留学咨询，是专业的日语学习机构。始终致力于为学员提供更优质、更高效的一站式日语学习及日本留学服务。目前，大成日语旗下开设有入门日语、等级日语、旅游日语、少儿日语、留学日语、商务日语等各种课程，能够满足有不同日语学习需求的各类人群。
+              </div>
+              <img src={require('./28profile_02.png')} className="about-img"></img>
+              <h2 className="about-title">日本留学专家</h2>
+              <div className="about-neirong">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;大成日语与日本东京、京都、大阪等地的多所优良语言学校有长期良好的合作，输送了许多优秀的中国学生赴日留学。针对于中学生、大学生、研究生等不同阶段的同学，制定有多种个性化的专业的留学申请方案。
+              </div>
           </div>
+          <div className="about-u">
+              <ul className="about-ul">
+                <li className="about-li">关于大成</li>
+                <Link to="/aboutDc"><li className="about-lis">大成介绍</li></Link>
+                <Link to="/aboutDc/about1"><li className="about-lis">团队介绍</li></Link>
+                <Link to="/aboutDc/about2"><li className="about-lis">教学环境</li></Link>
+              </ul>
+            </div>
+        </div>
       </div>
     )
   }
