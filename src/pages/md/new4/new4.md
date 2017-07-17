cfb6f7a7d1699283a929a97c9c12133c76eff1cf
magicly <magicly007@gmail.com>
2017-07-17 11:09:40 +0800

    webpack dynamic require

--- src/pages/md/new4/new4.md
+++ src/pages/md/new4/new4.md
@@ -4,7 +4,7 @@ draft: false
 tags: [rnn, neural network, dl, ml]
 category: ML
 date: "2017-04-10T15:42:58Z"
-cover: "18news_04.png"
+cover: "cover4.png"
 ---
 
 <iframe style="width: 100%; height: 5000px;border: none;" src="https://mp.weixin.qq.com/s?__biz=MjM5ODk3MTI3MQ==&mid=2653840215&idx=1&sn=d309efcbd5683f9db9cfb3b922b4cdf9&chksm=bd1851bf8a6fd8a94b9b4c07e56fc9a60fe018ffe57860efffc6ea367e3a156058af00c47b21&scene=0#rd" />
--- src/pages/news/index.jsx
+++ src/pages/news/index.jsx
@@ -9,25 +9,28 @@ class Index extends React.Component {
     return (
       <div className="news">
         <Helmet title="最新资讯" />
-        <p className="news-title"><img src={require('./news.png')}/>&nbsp;&nbsp;&nbsp;最新资讯</p>
+        <p className="news-title"><img src={require('./news.png')} />&nbsp;&nbsp;&nbsp;最新资讯</p>
         <ul
-            css={{
-              marginBottom: "30px",
-              marginTop:0,
-              marginLeft: 0,
-              listStyle: `none`,
-            }}
-          >
-            {posts.map(post =>
-              <li key={post.node.fields.slug}>
-                <Link to={post.node.fields.slug}>
-                  <img className="news-img" src={require('../' + post.node.frontmatter.cover.relativePath)} />
-                  <h3 className="news-titles">{post.node.frontmatter.title}</h3>
-                  <p className="news-desc">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{post.node.excerpt}</p>
-                </Link>
-              </li>
-            )}
-          </ul>
+          css={{
+            marginBottom: "30px",
+            marginTop: 0,
+            marginLeft: 0,
+            listStyle: `none`,
+          }}
+        >
+          {posts.map(post => {
+            let path = `${post.node.frontmatter.cover.relativePath}`;
+            path = path.slice(0, path.lastIndexOf('.'))
+            return <li key={post.node.fields.slug}>
+              <Link to={post.node.fields.slug}>
+                <img className="news-img" src={require('../' + path + '.png')} />
+                <h3 className="news-titles">{post.node.frontmatter.title}</h3>
+                <p className="news-desc">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{post.node.excerpt}</p>
+              </Link>
+            </li>
+          }
+          )}
+        </ul>
       </div>
     )
   }
@@ -38,7 +41,7 @@ export default Index;
 export const pageQuery = graphql`
 query NewsQuery {
   allMarkdownRemark(
-    limit: 3
+    limit: 4
     filter: { frontmatter: { draft: { ne: true } } }
     sort: {fields: [frontmatter___date], order: DESC}
     ) {
