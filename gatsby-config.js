23b748430dd8c9d7874abcd411ca199c53a401dd
magicly <magicly007@gmail.com>
2017-07-17 11:09:26 +0800

    install gatsby1.2.0

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,15 +18,6 @@ module.exports = {
           {
             resolve: `magicly-remark-target-new`,
           },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          `gatsby-remark-prismjs`,
-          "gatsby-remark-copy-linked-files",
-          "gatsby-remark-smartypants",
         ],
       },
     },
