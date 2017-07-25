ce5aacf47e124c577833915ae8c25a25409e7909
znq <znq@moondeMacBook-Pro.local>
2017-07-26 00:02:03 +0800

    首页侧边导航文字错误

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -64,7 +64,7 @@ class Index extends React.Component {
                   <Link to="/course"><li className="index-lis"><span><img className="index-lis-img" src={require('./course.png')}/><span>&nbsp;&nbsp;日语课程</span></span><span className="index-u-flex">コース紹介</span></li></Link>
                   <Link to="/liuxue"><li className="index-lis"><span><img className="index-lis-img" src={require('./oversea.png')}/><span>&nbsp;&nbsp;日本留学</span></span><span className="index-u-flex">日本留学</span></li></Link>
                   <Link to="/news"><li className="index-lis"><span><img className="index-lis-img" src={require('./news.png')}/><span>&nbsp;&nbsp;最新资讯</span></span><span className="index-u-flex">最新情報</span></li></Link>
-                  <Link to="/aboutus"><li className="index-lis"><span><img className="index-lis-img" src={require('./contact.png')}/><span>&nbsp;&nbsp;关于我们</span></span><span className="index-u-flex">コンタクト</span></li></Link>
+                  <Link to="/aboutus"><li className="index-lis"><span><img className="index-lis-img" src={require('./contact.png')}/><span>&nbsp;&nbsp;联系我们</span></span><span className="index-u-flex">コンタクト</span></li></Link>
               </ul>
           </div>
             : ''}
