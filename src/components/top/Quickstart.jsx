23bd8897940635a9124ecfe6f57b200637f7f966
magicly <magicly007@gmail.com>
2017-07-26 20:29:36 +0800

    styled-components

--- src/components/top/Quickstart.jsx
+++ src/components/top/Quickstart.jsx
@@ -1,64 +1,84 @@
-import './quickstart.css'
 import React from 'react';
 import Link from "gatsby-link"
-const Index = (props) => {
+import styled from 'styled-components'
+
+const CourseDiv = styled.div`
+  display: inline-block;
+  position: relative;
+`
+const CourseImg =  styled.img`
+  width: 355px;
+  height: 225px;
+  margin: 0 10px;
+  border: 1px solid #fff;
+`
+const CourseSpan= styled.span`
+  position: absolute;
+  left: 20px;
+  bottom: 20px;
+  background-color: rgba(0, 0, 0, 0.36);
+  color: #fff;
+  padding: 0 10px;
+  border: 1px solid #e5e5e5;
+  font-size: 0.7rem;
+  letter-spacing: 2px;
+`
+const Quickstart = (props) => {
   return <div css={{
     backgroundColor: '#ecede8',
-     width: "100%",
+    width: "100%",
     margin: "0 auto",
     marginTop: 140,
   }}>
-    <div className="quickstart">
-      <Link to="/course#ryrm">
-        <div className="quickstart-nr">
-          <img  className="quickstart-nr-img" src={require('./19pic_01.png')} />
-          <span className="quickstart-nr-span" >日语入门课程</span>
-        </div>
-      </Link>
-      <Link to="/course#rydj">
-        <div className="quickstart-nr">
-          <img  className="quickstart-nr-img" src={require('./20pic_02.png')} />
-          <span className="quickstart-nr-span" >日语等级课程</span>
-        </div>
-      </Link>
-      <Link to="/course#ky">
-        <div className="quickstart-nr">
-          <img  className="quickstart-nr-img" src={require('./21pic_03.png')} />
-          <span className="quickstart-nr-span" >口语课程</span>
-        </div>
-      </Link>
-      <Link to="/course#rylx">
-        <div className="quickstart-nr">
-          <img  className="quickstart-nr-img" src={require('./22pic_04.png')} />
-          <span className="quickstart-nr-span" >日语留学课程</span>
-        </div>
-      </Link>
-      <Link to="/course#xq">
-        <div className="quickstart-nr">
-          <img  className="quickstart-nr-img" src={require('./23pic_05.png')} />
-          <span className="quickstart-nr-span" >兴趣课程</span>
-        </div>
-      </Link>
-      <Link to="/course#ly">
-        <div className="quickstart-nr">
-          <img  className="quickstart-nr-img" src={require('./24pic_06.png')} />
-          <span className="quickstart-nr-span" >旅游日语</span>
-        </div>
-      </Link>
-      <Link to="/course#se">
-        <div className="quickstart-nr">
-          <img  className="quickstart-nr-img" src={require('./25pic_07.png')} />
-          <span className="quickstart-nr-span" >少儿日语</span>
-        </div>
-      </Link>
-      <Link to="/course#tbdz">
-        <div className="quickstart-nr">
-          <img  className="quickstart-nr-img" src={require('./26pic_08.png')} />
-          <span className="quickstart-nr-span" >特别定制课程</span>
-        </div>
-      </Link>
-    </div>
-  </div>
+    <Link to="/course#ryrm">
+      <CourseDiv>
+        <CourseImg src={require('./19pic_01.png')} />
+        <CourseSpan>日语入门课程</CourseSpan>
+      </CourseDiv>
+    </Link>
+    <Link to="/course#rydj">
+      <CourseDiv>
+        <CourseImg src={require('./20pic_02.png')} />
+        <CourseSpan>日语等级课程</CourseSpan>
+      </CourseDiv>
+    </Link>
+    <Link to="/course#ky">
+      <CourseDiv>
+        <CourseImg src={require('./21pic_03.png')} />
+        <CourseSpan>口语课程</CourseSpan>
+      </CourseDiv>
+    </Link>
+    <Link to="/course#rylx">
+      <CourseDiv>
+        <CourseImg src={require('./22pic_04.png')} />
+        <CourseSpan>日语留学课程</CourseSpan>
+      </CourseDiv>
+    </Link>
+    <Link to="/course#xq">
+      <CourseDiv>
+        <CourseImg src={require('./23pic_05.png')} />
+        <CourseSpan>兴趣课程</CourseSpan>
+      </CourseDiv>
+    </Link>
+    <Link to="/course#ly">
+      <CourseDiv>
+        <CourseImg src={require('./24pic_06.png')} />
+        <CourseSpan>旅游日语</CourseSpan>
+      </CourseDiv>
+    </Link>
+    <Link to="/course#se">
+      <CourseDiv>
+        <CourseImg src={require('./25pic_07.png')} />
+        <CourseSpan>少儿日语</CourseSpan>
+      </CourseDiv>
+    </Link >
+    <Link to="/course#tbdz">
+      <CourseDiv>
+        <CourseImg src={require('./26pic_08.png')} />
+        <CourseSpan>特别定制课程</CourseSpan>
+      </CourseDiv>
+    </Link>
+  </div >
 }
 
-export default Index;
+export default Quickstart;
--- src/pages/news/index.jsx
+++ src/pages/news/index.jsx
@@ -4,62 +4,62 @@ import Helmet from "react-helmet"
 import styled from 'styled-components'
 
 const NewsDiv = styled.div`
-    max-width: 1200px;
-    margin: 0 auto;
-    text-align: left;
-    background-color: #fff;
+  max-width: 1200px;
+  margin: 0 auto;
+  text-align: left;
+  background-color: #fff;
 `
 const Header = styled.p`
-    border-bottom: 1px solid #e5e5e5;
-    color: #666;
-    font-size: 0.7rem;
-    margin: 10px 0;
-    padding-bottom: 6px;
+  border-bottom: 1px solid #e5e5e5;
+  color: #666;
+  font-size: 0.7rem;
+  margin: 10px 0;
+  padding-bottom: 6px;
 `
 const HeaderImg = styled.img`
-    vertical-align: middle;
+  vertical-align: middle;
 `
 const Title = styled.h3`
-    color: #595757;
-    font-size: 1rem;
-    margin-bottom: 0;
+  color: #595757;
+  font-size: 1rem;
+  margin-bottom: 0;
 `
 const Desc = styled.p`
-    font-size: 0.7rem;
-    color: #595757;
+  font-size: 0.7rem;
+  color: #595757;
 `
 const Cover = styled.img`
-    width: 100%;
-    margin-bottom: 0;
+  width: 100%;
+  margin-bottom: 0;
 `
 const News = (props) => {
   const posts = props.data.allMarkdownRemark.edges
   return (
-    <NewsDiv>
-      <Helmet title="最新资讯" />
-      <Header><HeaderImg src={require('./news.png')} />&nbsp;&nbsp;&nbsp;<span>最新资讯</span></Header>
-      <ul
-        css={{
-          marginBottom: "30px",
-          marginTop: 0,
-          marginLeft: 0,
-          listStyle: `none`,
-        }}
-      >
-        {posts.map(post => {
-          let path = `${post.node.frontmatter.cover.relativePath}`;
-          path = path.slice(0, path.lastIndexOf('.'))
-          return <li key={post.node.fields.slug}>
-            <a href={post.node.frontmatter.link} target="_blank">
-              <Cover src={require('../' + path + '.png')} />
-              <Title>{post.node.frontmatter.title}</Title>
-              <Desc>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{post.node.excerpt}</Desc>
-            </a>
-          </li>
-        }
-        )}
-      </ul>
-    </NewsDiv>
+  <NewsDiv>
+    <Helmet title="最新资讯" />
+    <Header><HeaderImg src={require('./news.png')} />&nbsp;&nbsp;&nbsp;<span>最新资讯</span></Header>
+    <ul
+      css={{
+        marginBottom: "30px",
+        marginTop: 0,
+        marginLeft: 0,
+        listStyle: `none`,
+      }}
+    >
+      {posts.map(post => {
+        let path = `${post.node.frontmatter.cover.relativePath}`;
+        path = path.slice(0, path.lastIndexOf('.'))
+        return <li key={post.node.fields.slug}>
+          <a href={post.node.frontmatter.link} target="_blank">
+            <Cover src={require('../' + path + '.png')} />
+            <Title>{post.node.frontmatter.title}</Title>
+            <Desc>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{post.node.excerpt}</Desc>
+          </a>
+        </li>
+      }
+      )}
+    </ul>
+  </NewsDiv>
   )
 }
 
@@ -68,28 +68,28 @@ export default News;
 export const pageQuery = graphql`
 query NewsQuery {
   allMarkdownRemark(
-    limit: 4
-    filter: { frontmatter: { draft: { ne: true } } }
-    sort: {fields: [frontmatter___date], order: DESC}
-    ) {
-    edges {
-      node {
-        excerpt
-        fields {
-          slug
+  limit: 4
+  filter: { frontmatter: { draft: { ne: true } } }
+  sort: {fields: [frontmatter___date], order: DESC}
+  ) {
+  edges {
+    node {
+      excerpt
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+        date(formatString: "YYYY-MM-DD")
+        cover {
+          relativePath
+          base
         }
-        frontmatter {
-          title
-          date(formatString: "YYYY-MM-DD")
-          cover {
-            relativePath
-            base
-          }
-          img
-          link
-        }
+        img
+        link
       }
     }
+  }
   }
 }
 `
