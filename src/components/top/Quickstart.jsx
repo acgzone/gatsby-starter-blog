4126c2eefce7a40759ebf68617c8c09f92e5f534
znq <znq@moondeMacBook-Pro.local>
2017-07-26 23:16:52 +0800

    Quickstart 改为数组map

--- src/components/top/Quickstart.jsx
+++ src/components/top/Quickstart.jsx
@@ -24,60 +24,63 @@ const CourseSpan= styled.span`
   letter-spacing: 2px;
 `
 const Quickstart = (props) => {
+  const list = [
+    {
+      link: "/course#ryrm",
+      img: "19pic_01.png",
+      title: "日语入门课程"
+    },
+    {
+      link: "/course#rydj",
+      img: "20pic_02.png",
+      title: "日语等级课程"
+    },
+    {
+      link: "/course#ky",
+      img: "21pic_03.png",
+      title: "口语课程"
+    },
+    {
+      link: "/course#rylx",
+      img: "22pic_04.png",
+      title: "日语留学课程"
+    },
+    {
+      link: "/course#xq",
+      img: "23pic_05.png",
+      title: "兴趣课程"
+    },
+    {
+      link: "/course#ly",
+      img: "24pic_06.png",
+      title: "旅游日语"
+    },
+    {
+      link: "/course#se",
+      img: "25pic_07.png",
+      title: "少儿日语"
+    },
+    {
+      link: "/course#tbdz",
+      img: "26pic_08.png",
+      title: "特别定制课程"
+    },
+  ];
   return <div css={{
     backgroundColor: '#ecede8',
     width: "100%",
     margin: "0 auto",
     marginTop: 140,
   }}>
-    <Link to="/course#ryrm">
-      <CourseDiv>
-        <CourseImg src={require('./19pic_01.png')} />
-        <CourseSpan>日语入门课程</CourseSpan>
-      </CourseDiv>
-    </Link>
-    <Link to="/course#rydj">
-      <CourseDiv>
-        <CourseImg src={require('./20pic_02.png')} />
-        <CourseSpan>日语等级课程</CourseSpan>
-      </CourseDiv>
-    </Link>
-    <Link to="/course#ky">
-      <CourseDiv>
-        <CourseImg src={require('./21pic_03.png')} />
-        <CourseSpan>口语课程</CourseSpan>
-      </CourseDiv>
-    </Link>
-    <Link to="/course#rylx">
-      <CourseDiv>
-        <CourseImg src={require('./22pic_04.png')} />
-        <CourseSpan>日语留学课程</CourseSpan>
-      </CourseDiv>
-    </Link>
-    <Link to="/course#xq">
-      <CourseDiv>
-        <CourseImg src={require('./23pic_05.png')} />
-        <CourseSpan>兴趣课程</CourseSpan>
-      </CourseDiv>
-    </Link>
-    <Link to="/course#ly">
-      <CourseDiv>
-        <CourseImg src={require('./24pic_06.png')} />
-        <CourseSpan>旅游日语</CourseSpan>
-      </CourseDiv>
-    </Link>
-    <Link to="/course#se">
-      <CourseDiv>
-        <CourseImg src={require('./25pic_07.png')} />
-        <CourseSpan>少儿日语</CourseSpan>
-      </CourseDiv>
-    </Link >
-    <Link to="/course#tbdz">
-      <CourseDiv>
-        <CourseImg src={require('./26pic_08.png')} />
-        <CourseSpan>特别定制课程</CourseSpan>
-      </CourseDiv>
-    </Link>
+    {list.map(post => {
+      return <Link to={post.link}>
+        <CourseDiv>
+          <CourseImg src={require('./' + post.img)} />
+          <CourseSpan>{post.title}</CourseSpan>
+        </CourseDiv>
+      </Link>
+    }
+    )}
   </div >
 }
 
