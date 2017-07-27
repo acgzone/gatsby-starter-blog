72231cbb64bda3e8881546d470e5674786e7cb0c
znq <znq@moondeMacBook-Pro.local>
2017-07-27 17:05:01 +0800

    联系我们页面加二维码

--- src/pages/aboutus/index.jsx
+++ src/pages/aboutus/index.jsx
@@ -13,22 +13,38 @@ const Index = () => {
 				</div>
 				<div className="aboutus-nr">
 					<div className="aboutus-khbz">
+						<h2 className="aboutus-h2">关注我们</h2>
+						<img src={require('./cont_sjx.png')} className="aboutus-xs"/>
+					</div>
+				</div>
+				<div className="aboutus-wx">
+					<div className="aboutus-wx-div">
+						<img className="aboutus-wx-img" src={require('./gzh.png')}/>
+						<span>微信公众号</span>
+					</div>
+					<div className="aboutus-wx-div">
+						<img className="aboutus-wx-img" src={require('./zx.jpeg')}/>
+						<span>咨询微信</span>
+					</div>
+				</div>
+				<div className="aboutus-nr">
+					<div className="aboutus-khbz">
 						<h2 className="aboutus-h2">客户帮助</h2>
 						<img src={require('./cont_sjx.png')} className="aboutus-xs"/>
 					</div>
 				</div>
-				<div className="aboutus-xx">座机：028 – 84278169</div>
+				<div className="aboutus-xx aboutus-marginT">座机：028 – 84278169</div>
 				<div className="aboutus-xx">联系电话：17713547616（颜兴）</div>
 				<div className="aboutus-xx">QQ：396703883</div>
-				<div className="aboutus-xx">邮箱：396703883@qq.com</div>
+				<div className="aboutus-xx aboutus-marginB">邮箱：396703883@qq.com</div>
 				<div className="aboutus-nr">
 					<div className="aboutus-khbz">
 						<h2 className="aboutus-h2">联系地址</h2>
 						<img src={require('./cont_sjx.png')} className="aboutus-xs"/>
 					</div>
 				</div>
-				<div className="aboutus-xx">联系地址：成都市锦江区宏济新路5号  SOHO商务港309</div>
-				<div className="aboutus-xx">地铁：地铁2号线牛王庙站  出站5分钟即达</div>
+				<div className="aboutus-xx aboutus-marginT">联系地址：成都市锦江区宏济新路5号  SOHO商务港309</div>
+				<div className="aboutus-xx aboutus-marginB">地铁：地铁2号线牛王庙站  出站5分钟即达</div>
 				
     </div>
   )
