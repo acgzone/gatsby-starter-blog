5b2a95a80bc2ba4f3c4de01ae8292c7d59e4da93
magicly <magicly007@gmail.com>
2017-08-04 21:23:37 +0800

    home sidebar absolute

---
+++ src/components/home/Sidebar.jsx
@@ -0,0 +1,18 @@
+import Link from "gatsby-link"
+
+const Sidebar = ({ scrollTop }) => {
+  console.log(scrollTop)
+  const top = scrollTop - 500 > 50 ? scrollTop - 500 : 50;
+  return <div className="index-u" css={{ top }}>
+    <ul className="index-ul">
+      <li className="index-li"><span><img className="index-lis-img" src={require('./index.png')} /><span>&nbsp;&nbsp;首页</span></span><span className="index-u-flex1">ホーム</span></li>
+      <Link to="/aboutDc"><li className="index-lis"><span><img className="index-lis-img" src={require('./about-us.png')} /><span>&nbsp;&nbsp;关于大成</span></span><span className="index-u-flex">学院案内</span></li></Link>
+      <Link to="/course"><li className="index-lis"><span><img className="index-lis-img" src={require('./course.png')} /><span>&nbsp;&nbsp;日语课程</span></span><span className="index-u-flex">コース紹介</span></li></Link>
+      <Link to="/liuxue"><li className="index-lis"><span><img className="index-lis-img" src={require('./oversea.png')} /><span>&nbsp;&nbsp;日本留学</span></span><span className="index-u-flex">日本留学</span></li></Link>
+      <Link to="/news"><li className="index-lis"><span><img className="index-lis-img" src={require('./news.png')} /><span>&nbsp;&nbsp;新闻资讯</span></span><span className="index-u-flex">ニュース</span></li></Link>
+      <Link to="/aboutus"><li className="index-lis"><span><img className="index-lis-img" src={require('./contact.png')} /><span>&nbsp;&nbsp;联系我们</span></span><span className="index-u-flex">コンタクト</span></li></Link>
+    </ul>
+  </div>
+}
+
+export default Sidebar;
--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import Helmet from "react-helmet"
 
 import Banner from '../Banner'
+import Sidebar from './Sidebar'
 import Course from './Course'
 import Liuxue from './Liuxue'
 import News from './News'
-import Link from "gatsby-link"
 
 import './index.css'
 
@@ -13,7 +13,7 @@ class Index extends React.Component {
   constructor() {
     super();
     this.state = {
-      showSideBar: false,
+      scrollTop: 0,
     }
     this.handleScroll = this.handleScroll.bind(this);// 直接把this.handleScroll.bind(this);写在addEventListener和removeEventListener里面的话，会导致两次bind(this)的不是同一个函数，会remove不掉，存在内存泄露
   }
@@ -26,17 +26,9 @@ class Index extends React.Component {
   }
   handleScroll(e) {
     const scrollTop = this.getScrollTop();
-    // console.log('scroll...', scrollTop)
-    // todo 缩放比例会影响这个值， 应该也要“自适应”屏幕大小
-    if (scrollTop > 440) {
-      this.setState({
-        showSideBar: true,
-      })
-    } else {
-      this.setState({
-        showSideBar: false,
-      })
-    }
+    this.setState({
+      scrollTop,
+    })
   }
   getScrollTop() {
     var scrollTop = 0;
@@ -56,18 +48,7 @@ class Index extends React.Component {
         <Helmet title={siteTitle} />
         <Banner />
         <div id="main">
-          {this.state.showSideBar ?
-            <div className="index-u">
-              <ul className="index-ul">
-                  <li className="index-li"><span><img className="index-lis-img" src={require('./index.png')}/><span>&nbsp;&nbsp;首页</span></span><span className="index-u-flex1">ホーム</span></li>
-                  <Link to="/aboutDc"><li className="index-lis"><span><img className="index-lis-img" src={require('./about-us.png')}/><span>&nbsp;&nbsp;关于大成</span></span><span className="index-u-flex">学院案内</span></li></Link>
-                  <Link to="/course"><li className="index-lis"><span><img className="index-lis-img" src={require('./course.png')}/><span>&nbsp;&nbsp;日语课程</span></span><span className="index-u-flex">コース紹介</span></li></Link>
-                  <Link to="/liuxue"><li className="index-lis"><span><img className="index-lis-img" src={require('./oversea.png')}/><span>&nbsp;&nbsp;日本留学</span></span><span className="index-u-flex">日本留学</span></li></Link>
-                  <Link to="/news"><li className="index-lis"><span><img className="index-lis-img" src={require('./news.png')}/><span>&nbsp;&nbsp;新闻资讯</span></span><span className="index-u-flex">ニュース</span></li></Link>
-                  <Link to="/aboutus"><li className="index-lis"><span><img className="index-lis-img" src={require('./contact.png')}/><span>&nbsp;&nbsp;联系我们</span></span><span className="index-u-flex">コンタクト</span></li></Link>
-              </ul>
-          </div>
-            : ''}
+          <Sidebar scrollTop={this.state.scrollTop} />
           <div id="content" css={{
             marginLeft: 20,
             marginRight: 230,
